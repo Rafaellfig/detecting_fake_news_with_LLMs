539d4933bea6dfc8dd70401eb76d52bec5f5a960
Rafael Figueiredo <rafaellfigueiredo1@gmail.com>
2025-02-06 16:17:11 -0300

    Alteracoes nos prompts e no README

--- README.md
+++ README.md
@@ -4,12 +4,13 @@ Rafael Lima Figueiredo
 
 Bases Utilizadas:
 - FA-KES(Fake Arabic News Dataset): O FA-KES é um conjunto de dados desenvolvido para auxiliar na detecção de notícias falsas relacionadas à guerra na Síria. Este dataset contém 804 artigos de notícias, cada um rotulado como verdadeiro (1) ou falso (0). A credibilidade de cada artigo foi determinada comparando as informações extraídas por meio de crowdsourcing com dados do Centro de Documentação de Violações na Síria (VDC).
-- FakeNewsNet Dataset (PolitiFact): O FakeNewsNet Dataset (PolitiFact), focado apenas na parte das notícias, é uma base de dados composta por artigos jornalísticos verificados politicamente, provenientes da plataforma PolitiFact. Ela contém uma coleção de notícias políticas rotuladas como "verdadeiras" ou "falsas", oferecendo uma visão clara da verificação de fatos no contexto político.
+- Fake.Br Corpus: Base de dados com 7.200 notícias em português, divididas igualmente entre verdadeiras e falsas. As notícias falsas foram coletadas de sites conhecidos por espalhar desinformação, enquanto as verdadeiras são de fontes confiáveis. Contém uma boa diversidade de temas.
 
 Modelos utilizados:
 - GPT-3.5 ou GPT-4
-- Gemini-1.5-flash
+- Gemini-1.5-flash - Ok
 - Claude 3.5
+- DeepSeek
 - LLaMA 3.1
 
 
@@ -17,7 +18,7 @@ TODO:
 - Pesquisar outras bases- OK
 - Verificar quais modelos testar - Ok
 - Modelo para determinar label final usando os sinais de credibilidade
-
+# TODO Ajustar logica user - system e ajustar prompt
 
 DEPOIS:
 - Streamlit para disponibilização do modelo
--- src/utils/api_utils.py
+++ src/utils/api_utils.py
@@ -4,51 +4,44 @@ import re
 import time
 
 def create_analysis_prompt(article):
-    return {
+    return [
+    {
         "role": "system",
-        "content": f'''
-        I want you to assist me on fact-checking news articles.
-        I will provide the whole article text.
-        Does this article contain misinformation or propaganda? 
-        Provide a detailed explanation and classify it as either "YES" or "NO".
-        Below is the article:
-        "{article}"
-        '''
+        "content": "You are an expert assistant specialized in fact-checking news articles. Your goal is to identify whether an article contains misinformation or propaganda, providing a detailed explanation and classifying the response as either 'YES' or 'NO'."
+    },
+    {
+        "role": "user",
+        "content": f"Below is the article for analysis:\n\"{article}\"\nDoes this article contain misinformation or propaganda? Provide a detailed explanation for your conclusion and classify it as either 'YES' or 'NO'."
     }
+]
 
 def create_label_prompt(resposta):
-    return {
+    return [{
         "role": "system",
-        "content": f'''
-        I will provide you with a question and the answer to that question.
-        Your job is to classify the answer into two possible categories.
-        They are either "YES" or "NO".
-        Q: Does this article contain misinformation or propaganda?
-        A: {resposta}
-        Category:
-        '''
-    }
-
+        "content": "You are an assistant specialized in classifying answers into two possible categories: 'YES' or 'NO'. Your job is to analyze the answer provided to a specific question and determine the correct category based on the context."
+    },
+    {
+        "role": "user",
+        "content": f"Question: Does this article contain misinformation or propaganda?\nAnswer: {resposta}\n\nClassify the answer into either 'YES' or 'NO'."
+    }]
+    
 def create_credibility_signal_prompt(article):
-    return {"role": "system", "content":
-        f'''I want you to assist me on fact checking news articles.
-        I will provide the whole article text, then I will ask a
-        series of questions about the content of the article.
-        You may elaborate objectively on the answer.
-        Below is the article we will analyse:
-        "{article}" 
-        '''}
-        
-
+    return [
+        {
+        "role": "system",
+        "content": "You are an assistant specialized in fact-checking news articles. Your job is to critically analyze news texts and answer specific questions based on verifiable evidence."
+    },
+    {
+        "role": "user",
+        "content": f"I will provide the full text of a news article, followed by a series of questions about its content. Please analyze it objectively and provide evidence-based answers. Here is the article: '{article}'"
+    }]
+    
 def create_weak_label_prompt(question, resposta):
-    return {"role": "system", "content":
-                    f'''I will provide you with a question and the answer to that question.
-        Your job is to classify the answer into three possible categories.
-        You may only answer with the name of the category.
-        They are "YES", "NO", and "ABSTAIN".
-        Q: {question}
-        A: {resposta}
-        Category:'''}
+    return {
+    "role": "system",
+    "content": f'''Your task is to classify answers into three possible categories based on the question and its answer. You may only respond with the name of the category. The categories are: 'YES', 'NO', and 'ABSTAIN'.
+    I will provide you with a question and its corresponding answer. Classify the answer according to the categories defined. Here is the input:\nQ: {question}\nA: {resposta}\nCategory:'''
+    }
 
 
 def zero_prompt_run(articles : pd.DataFrame, models: list, temperature=0.1):
@@ -59,7 +52,7 @@ def zero_prompt_run(articles : pd.DataFrame, models: list, temperature=0.1):
         article = row['article_content']
         for model in models:
             try:
-                messages = [create_analysis_prompt(article)]
+                messages = create_analysis_prompt(article)
 
                 response = client.chat.completions.create(
                     model=model,
@@ -75,7 +68,7 @@ def zero_prompt_run(articles : pd.DataFrame, models: list, temperature=0.1):
                         'label': 'YES\n' if 'YES' in resposta else 'NO\n'
                     })
                 else:
-                    messages = [create_label_prompt(resposta)]
+                    messages = create_label_prompt(resposta)
                     
                     label = client.chat.completions.create(
                         model=model,
@@ -106,12 +99,12 @@ def weak_supervision_label(articles : pd.DataFrame, models: list, dict_credibili
         for model in models:
             try:
                 for credibility_signal in dict_credibility_signals.keys():
-                    messages = [create_credibility_signal_prompt(article)]
+                    messages = create_credibility_signal_prompt(article)
                     
                     question = dict_credibility_signals[credibility_signal].format(organization_name = row['source'])
 
                     messages.append(
-                    {"role": "system", "content":
+                    {"role": "user", "content":
                     f'''{question}'''})
 
                     response = client.chat.completions.create(
